d7d9c28f3b3a3a17403b6fddfb07b0775b378398
liMoHa <dudgk6382@gmail.com>
2021-12-01 18:48:55 +0900

    JS: filter

--- mini-shopping-mall/src/main.js
+++ mini-shopping-mall/src/main.js
@@ -1,5 +1,4 @@
 // fetch items in itesm.json
-
 function loadItems(){
     return fetch('../data/items.json')
     .then((response) => {
@@ -10,6 +9,7 @@ function loadItems(){
     });
 }
 
+// 함수를 적는 순서도 확인해보자.
 function itemArr(item){
     return `<li>
                 <img src="${item['item']}" alt="cloth-image" />
@@ -18,13 +18,30 @@ function itemArr(item){
 }
 
 function displayItems(items){
+    console.log("item:",items);
     let lists = document.querySelector('.lists');
     lists.innerHTML= items.map(item => itemArr(item)).join('');
 }
 
+function filterCloths(e){
+    if(e.target.dataset.type === 'btn'){
+        itemsPromise.then((items) => {
+            let filteredItems = items.filter((item) => {
+                if(e.target.dataset.id == "all") return true;
+                else{
+                    return item['type'] === e.target.dataset.id || item['color'] === e.target.dataset.id;
+                }
+            });
+            console.log("?:",filteredItems);
+            displayItems(filteredItems); // display filtered clothes
+        });
+    }
+}
 /* items 받아오기 */
-loadItems()
-.then((items) => {
-    console.log(items);
-    displayItems(items);
-});
+// item을 한 번만 받아온 후로 계속 사용하고 싶어서 아래와 같이 쓴 건데 이렇게 사용하지 않을 수도 있으니까 이 부분 확인해보자.
+const itemsPromise = loadItems(); // 나중에 사용할 거임. 
+
+itemsPromise.then(displayItems); // at first load
+
+const body = document.querySelector('body');
+body.addEventListener('click', filterCloths);
